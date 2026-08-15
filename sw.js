db46ea6669796037b0d8fb047770d2c9e96ecc91
DB-8442 <marco-petersen@outlook.com>
2026-08-15 15:00:56 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE="ki-ausbilder-v2";const ASSETS=["./","./index.html","./manifest.webmanifest"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
